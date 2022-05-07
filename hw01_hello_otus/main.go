18c51f8bf9782a270db234c5b9073adaff6c263a
alex <alver@angelsit.ru>
2022-05-07 19:46:54 +0300

    HW1 is completed

--- hw01_hello_otus/main.go
+++ hw01_hello_otus/main.go
@@ -1,5 +1,17 @@
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"golang.org/x/example/stringutil"
+	"os"
+)
+
 func main() {
-	// Place your code here.
+	r := bufio.NewReader(os.Stdin)
+	b, _, err := r.ReadLine()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(stringutil.Reverse(string(b)))
 }
