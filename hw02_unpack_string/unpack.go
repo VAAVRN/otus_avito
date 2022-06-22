df237f70d035cc83d7204798d0a8f6dc9d450a0b
alex <alver@angelsit.ru>
2022-06-22 23:39:37 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,81 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+const e = '\\'
+
+func Unpack(s string) (string, error) {
+	if s == "" {
+		return "", nil
+	}
+
+	rs := []rune(s)
+	if unicode.IsDigit(rs[0]) { // краевой случай цифра в начале строки
+		return "", ErrInvalidString
+	}
+	lnR := len(rs)
+	prev, prevIsEsc, err := getNext(rs, 0)
+	if err != nil {
+		return "", err
+	}
+
+	var res strings.Builder
+	i := 1
+	if prevIsEsc {
+		i++
+	}
+	for ; i < lnR; i++ {
+
+		cur, curIsEsc, err := getNext(rs, i)
+		if err != nil {
+			return "", err
+		}
+
+		if curIsEsc {
+			i++
+		}
+		if unicode.IsDigit(cur) {
+			if !curIsEsc && !prevIsEsc && unicode.IsDigit(prev) {
+				return "", ErrInvalidString
+			}
+			if curIsEsc {
+				res.WriteRune(prev)
+			} else {
+				cnt, _ := strconv.Atoi(string(cur))
+				res.WriteString(strings.Repeat(string(prev), cnt))
+			}
+		} else if prevIsEsc || !unicode.IsDigit(prev) {
+			res.WriteRune(prev)
+		}
+		prev, prevIsEsc = cur, curIsEsc
+	}
+
+	if !unicode.IsDigit(prev) || prevIsEsc {
+		res.WriteRune(prev)
+	}
+	return res.String(), nil
+}
+
+func getNext(rs []rune, i int) (r rune, isEsc bool, err error) {
+	if i > len(rs)-1 {
+		err = ErrInvalidString
+		return
+	}
+	if rs[i] == e {
+		if i >= len(rs)-1 {
+			err = ErrInvalidString
+			return
+		}
+		r = rs[i+1]
+		isEsc = true
+	} else {
+		r = rs[i]
+	}
+	return
 }
--- hw02_unpack_string/unpack_test.go
+++ hw02_unpack_string/unpack_test.go
@@ -2,9 +2,8 @@ package hw02unpackstring
 
 import (
 	"errors"
-	"testing"
-
 	"github.com/stretchr/testify/require"
+	"testing"
 )
 
 func TestUnpack(t *testing.T) {
@@ -16,11 +15,35 @@ func TestUnpack(t *testing.T) {
 		{input: "abccd", expected: "abccd"},
 		{input: "", expected: ""},
 		{input: "aaa0b", expected: "aab"},
-		// uncomment if task with asterisk completed
-		// {input: `qwe\4\5`, expected: `qwe45`},
-		// {input: `qwe\45`, expected: `qwe44444`},
-		// {input: `qwe\\5`, expected: `qwe\\\\\`},
-		// {input: `qwe\\\3`, expected: `qwe\3`},
+		//uncomment if task with asterisk completed
+		{input: `qwe\4\5`, expected: `qwe45`},
+		{input: `qwe\45`, expected: `qwe44444`},
+		{input: `qwe\\5`, expected: `qwe\\\\\`},
+		{input: `qwe\\\3`, expected: `qwe\3`},
+	}
+
+	for _, tc := range tests {
+		tc := tc
+		t.Run(tc.input, func(t *testing.T) {
+			result, err := Unpack(tc.input)
+			require.NoError(t, err)
+			require.Equal(t, tc.expected, result)
+		})
+	}
+}
+
+func TestUnpackAdditional(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected string
+	}{
+		{input: "d\n5abc", expected: "d\n\n\n\n\nabc"},
+		{input: "d/////5abc", expected: "d/////////abc"},
+		{input: "   4", expected: "      "},
+		//uncomment if task with asterisk completed
+		{input: `qwe\\`, expected: `qwe\`},
+		{input: `\\\12`, expected: `\11`},
+		{input: `\\\\`, expected: `\\`},
 	}
 
 	for _, tc := range tests {
@@ -35,6 +58,17 @@ func TestUnpack(t *testing.T) {
 
 func TestUnpackInvalidString(t *testing.T) {
 	invalidStrings := []string{"3abc", "45", "aaa10b"}
+	for _, tc := range invalidStrings {
+		tc := tc
+		t.Run(tc, func(t *testing.T) {
+			_, err := Unpack(tc)
+			require.Truef(t, errors.Is(err, ErrInvalidString), "actual error %q", err)
+		})
+	}
+}
+
+func TestUnpackInvalidStringAdditional(t *testing.T) {
+	invalidStrings := []string{`\`, `a\`, `\\\`, `1\`}
 	for _, tc := range invalidStrings {
 		tc := tc
 		t.Run(tc, func(t *testing.T) {
